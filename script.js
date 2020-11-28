017d7dfbdb560b1766886fce8f084e2dffbb588c
Denis Trofimenko <trofimenkod33@gmail.com>
2020-11-28 16:59:24 +0300

    created general branch

---
+++ script.js
@@ -0,0 +1,16 @@
+function consol() {
+    alert("os the type of a parameter");
+    
+}
+consol();
+
+/*создал функцию с названием consol , затем вызвал ее */
+
+function showMi(comparision) {
+    alert (1===3) ;
+    
+}
+
+showMi();
+
+/*создал функцию сравнения строгово потом вызвал  */
