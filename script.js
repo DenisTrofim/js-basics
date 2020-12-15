7ccc4664c55dc925b412c0c509fb6decb7cf3df2
Denis Trofimenko <trofimenkod33@gmail.com>
2020-12-15 21:49:13 +0300

    first commit

--- script.js
+++ script.js
@@ -16,3 +16,40 @@ function showMi(y) {
 }
 
 showMi();
+
+
+function Function1(x) {
+    console.log (a = 15);
+    console.log (b = 6);
+    alert(a + b);
+    console.log(input1()) ;
+    
+
+}
+
+Function1;
+
+//function phone1(y) {
+//    console.log (phone1) ;
+//}
+//
+//function email1(z) {
+//    console.log (Text) ;
+//}
+//email1();
+
+//const input = document.querySelector("input")
+//const button = document.querySelector("button")
+//
+//const handleClick = () => {
+//  const value = input.value.trim()
+//  if (value) {
+//    alert(`Мое число: ${value}`)
+//  } else {
+//    console.log("введите число")
+//  }  
+//}
+//
+//button.addEventListener("click", handleClick)
+
+
