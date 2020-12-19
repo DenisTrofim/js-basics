eadfbec067552156e939ecf8df4925e21c21947e
Denis Trofimenko <trofimenkod33@gmail.com>
2020-12-19 16:20:11 +0300

    version option

--- script.js
+++ script.js
@@ -1,55 +1,30 @@
-function returnTypeTwo(x) {
-    console.log(typeof x + '1');
-    console.log(typeof x + '2');
-    console.log(x)
-    
-}
+//function returnTypeTwo(x) {
+//    console.log(typeof x + '1');
+//    console.log(typeof x + '2');
+//    console.log(x)
+//    
+//}
 
-returnTypeTwo(3);
-
-
-
-
-function showMi(y) {
-   console.log(returnTypeTwo === showMi)
-
-}
-
-showMi();
-
-
-function Function1(x) {
-    console.log (a = 15);
-    console.log (b = 6);
-    alert(a + b);
-    console.log(input1()) ;
-    
-
-}
-
-Function1;
-
-//function phone1(y) {
-//    console.log (phone1) ;
+//returnTypeTwo(3);
+//
+//function form-id(x){
+//    console.log('#form-id');
 //}
 //
-//function email1(z) {
-//    console.log (Text) ;
+//form-id();
+
+//function submit(x) {
+//  console.log('#form\\-id') ;
+//  document.querySelector('#form\\:id');
 //}
-//email1();
-
-//const input = document.querySelector("input")
-//const button = document.querySelector("button")
-//
-//const handleClick = () => {
-//  const value = input.value.trim()
-//  if (value) {
-//    alert(`Мое число: ${value}`)
-//  } else {
-//    console.log("введите число")
-//  }  
-//}
-//
-//button.addEventListener("click", handleClick)
 
 
+function buton (x) {
+  console.log('.some_name');
+  document.querySelector('some_name');
+  var special = document.querySelector( "some_name,some_name2,some_name3" );
+  var select = document.querySelectorAll("div.main-block");
+  var matches = document.body.querySelectorAll('h3');
+}
+
+buton();
